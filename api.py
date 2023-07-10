a5e9e580a0ee0082511d1a0e5045b203085135ad
Antoine Poellhuber <64378571+Cloudsky01@users.noreply.github.com>
2023-07-10 10:48:19 -0400

    feat/ renamed file + deleted controller

---
+++ api.py
@@ -0,0 +1,29 @@
+from vehicles import VehicleGroup
+from utils import callAPI, write_to_file, Coordinates
+import time
+from vehicles import Vehicle, VehicleDetails
+
+
+def fetch_all_vehicles(lat, lng) -> VehicleGroup:
+    vehicle_group = VehicleGroup([])
+    response = callAPI("https://restapifrontoffice.reservauto.net/api/v2/Vehicle/FreeFloatingAvailability?CityId=59")
+    log_info(response.json())
+    for vehicle in response.json()['vehicles']:
+        vehicle = Vehicle(vehicle['vehicleId'], Coordinates(vehicle['vehicleLocation']))
+        vehicle.calculate_distance(lat, lng)
+        vehicle_group.add_vehicle(vehicle)
+    
+    return vehicle_group
+
+def fetch_all_stations_vehicles():
+    return callAPI("https://restapifrontoffice.reservauto.net/api/v2/Station?BranchId=1&CityId=59")
+
+
+def fetch_vehicle_details(vehicle_id):
+    response = callAPI(f"https://restapifrontoffice.reservauto.net/api/v2/Vehicle/{vehicle_id}/FreeFloating?BranchId=1")
+    return VehicleDetails(response.json()).get_formatted_details()
+
+def log_info(log):
+    log_datetime = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime())
+    write_to_file(f"./data/log_{log_datetime}.json", log)
+
